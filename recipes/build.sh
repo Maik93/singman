89c54e3c302390f74b949b2b7c36a903adf92d56
Michael Mugnai <michael.mugnai@gmail.com>
2022-12-09 18:57:48 +0100

    minors

--- recipes/build.sh
+++ recipes/build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 set -e
 
-[[ $# -ne 1 ]] && exit 2
+[[ $# -lt 1 ]] && exit 2
 
 RECIPE_NAME=$1
+
+if [[ $# -eq 2 ]]; then
+	IMAGE_NAME=$2
+else
+	IMAGE_NAME=$1
+fi
 
 SCRIPT_PATH=`( cd "$(dirname "$0")" && pwd -P )`
 IMAGE_PATH=`( cd "$SCRIPT_PATH/../images" && pwd -P )`
 
 [[ ! -f $SCRIPT_PATH/$RECIPE_NAME.def ]] && exit 1
 
-singularity build --fakeroot --fix-perms -F $IMAGE_PATH/$RECIPE_NAME.sif $SCRIPT_PATH/$RECIPE_NAME.def
+singularity build --fakeroot --fix-perms -F $IMAGE_PATH/$IMAGE_NAME.sif $SCRIPT_PATH/$RECIPE_NAME.def
